964f8e92e0feb029b109b800df9014bad8e04dae
Ivan Sison <ifsison-allcard@allcardtech.com.ph>
2021-01-13 15:53:42 +0800

    Initial Commit

---
+++ app/src/main/java/com/allcardtech/lib/android_dropdown_field/MainActivity.java
@@ -0,0 +1,14 @@
+package com.allcardtech.lib.android_dropdown_field;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
